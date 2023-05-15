15789fd5106ef8de2a98c26b2a61c3483552c741
Kerjo <1683284@cstj.qc.ca>
2023-05-15 12:58:15 -0400

    TP Complet avec ReadMe.md

--- app/src/main/java/com/example/tenretni/ui/networks/NetworksFragment.kt
+++ app/src/main/java/com/example/tenretni/ui/networks/NetworksFragment.kt
@@ -28,10 +28,6 @@ class NetworksFragment : Fragment() {
         _binding = FragmentNetworksBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textNotifications
-        networksViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
     }
 
